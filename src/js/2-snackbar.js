2035ad105ddaaeb016a5abd51032c53a980fd745
xxLEITTOxx <xxleittoxx@gmail.com>
2025-07-06 20:55:25 +0200

    logic fix

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,18 +3,20 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const myForm = document.querySelector(".form");
 const myDelay = document.querySelector('input[name="delay"]');
-const promiseFullfilled = document.querySelector('input[value="fulfilled"]');
 
 myForm.addEventListener("submit", createPromise);
 
 function createPromise(event) {
   event.preventDefault();
   const delay = Number(myDelay.value);
-  const isFulfilled = promiseFullfilled.checked;
+  const isFulfilled = myForm.elements.state;
+  const promiseState = isFulfilled.value;
+  // console.log(promiseState);
+
   myForm.reset();
   let myBestPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (isFulfilled) {
+      if (promiseState === "fulfilled") {
         resolve(delay);
       } else {
         reject(delay);
